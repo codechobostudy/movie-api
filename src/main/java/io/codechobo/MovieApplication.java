071fc26ae2a182fe34065106a626118d46065d83
Changhwa Oh <changhwaoh.co@gmail.com>
2016-07-26 05:42:11 +0900

    Init Project

---
+++ src/main/java/io/codechobo/MovieApplication.java
@@ -0,0 +1,12 @@
+package io.codechobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+}
---
+++ src/test/java/io/codechobo/MovieApplicationIntegrationTest.java
@@ -0,0 +1,18 @@
+package io.codechobo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles(value = "test")
+public class MovieApplicationIntegrationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
