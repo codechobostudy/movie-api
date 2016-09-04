5a8962fe5cc615bec7e014f1253bea8136bcc7d4
urosaria <urosaria0720@gmail.com>
2016-09-04 10:17:07 +0900

    feat: 리뷰, 평점 도메인 api 기본

--- src/main/java/io/codechobo/review/domain/repository/GradeRepository.java
+++ src/main/java/io/codechobo/review/domain/repository/GradeRepository.java
@@ -3,7 +3,9 @@ package io.codechobo.review.domain.repository;
 
 import io.codechobo.review.domain.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GradeRepository extends JpaRepository<Grade, Long> {
 
 }
--- src/main/java/io/codechobo/review/domain/repository/ReviewRepository.java
+++ src/main/java/io/codechobo/review/domain/repository/ReviewRepository.java
@@ -2,7 +2,9 @@ package io.codechobo.review.domain.repository;
 
 import io.codechobo.review.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
